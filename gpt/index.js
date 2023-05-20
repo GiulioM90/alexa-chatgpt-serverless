9d3ed1974f96986dc24327fcc7bc1d2730db4ac3
GiulioMollo <g.mollo@shopfully.com>
2023-05-20 18:09:38 +0200

    first commit

---
+++ gpt/index.js
@@ -0,0 +1,122 @@
+import { Configuration, OpenAIApi } from 'openai';
+const Alexa = require('ask-sdk-core');
+
+const keys = process.env.API_KEY_GPT;
+
+const configuration = new Configuration({
+    apiKey: keys,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    const speechText = 'Ciao, siamo entrati nell ambiente di chat G. P. T. , chiedi quello che vuoi!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard('Ciao, siamo entrati nell ambiente di chat G. P. T. , chiedi quello che vuoi!', speechText)
+      .getResponse();
+  }
+};
+const ChatGPTIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'ChatGPTIntent';
+    },
+    async handle(handlerInput) {
+        const question = Alexa.getSlotValue(handlerInput.requestEnvelope, 'question');
+
+        const response = await openai.createCompletion({
+            model: 'text-davinci-003',
+            prompt: question,
+            max_tokens: 99,
+            top_p: 1,
+        });
+        const speakOutput = response.data.choices[0].text.replace(/\n/g, "");
+        console.log(typeof speakOutput);
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt('What else can I help with?')
+            .getResponse();
+    }
+};
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+      && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'You can ask me the weather!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard('You can ask me the weather!', speechText)
+      .getResponse();
+  }
+};
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+      && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+        || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const speechText = 'Goodbye!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard('Goodbye!', speechText)
+      .withShouldEndSession(true)
+      .getResponse();
+  }
+};
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    // Any clean-up logic goes here.
+    return handlerInput.responseBuilder.getResponse();
+  }
+};
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, I don\'t understand your command. Please say it again.')
+      .reprompt('Sorry, I don\'t understand your command. Please say it again.')
+      .getResponse();
+  }
+};
+let skill;
+
+exports.handler = async function (event, context) {
+  console.log(`REQUEST++++${JSON.stringify(event)}`);
+  if (!skill) {
+    skill = Alexa.SkillBuilders.custom()
+      .addRequestHandlers(
+        LaunchRequestHandler,
+        ChatGPTIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        SessionEndedRequestHandler,
+      )
+      .addErrorHandlers(ErrorHandler)
+      .create();
+  }
+
+  const response = await skill.invoke(event, context);
+  console.log(`RESPONSE++++${JSON.stringify(response)}`);
+
+  return response;
+};
